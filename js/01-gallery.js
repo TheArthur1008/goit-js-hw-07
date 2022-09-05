6d828ad5d194ebb191b8fefd2cf95c51b07d7c8c
Arthur Korostelev <korostelev.art.ua@gmail.com>
2022-09-05 15:07:35 +0300

    create gallery and ligth-box

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,60 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+function makeGalleryImgMarkup({ preview, original, description }) {
+    return `<div class="gallery-item">
+        <a class="gallery-link" href="${original}">
+            <img class="galerry-img" src="${preview}" data-source="${original}" alt="${description}"/>
+        </a>
+    </div>`;
+};
+
+function makeGalleryMarkup(array) {
+    return array.map(makeGalleryImgMarkup).join('');
+};
+
+function renderMarkup(markup) {
+	galleryEl.insertAdjacentHTML('afterbegin', markup);
+};
+
+const galleryMarkup = makeGalleryMarkup(galleryItems);
+renderMarkup(galleryMarkup);
+
+galleryEl.addEventListener('click', gellaryImgClick);
+
+function gellaryImgClick(element) {
+	if (element.target.nodeName !== 'IMG') {
+		return;
+	}
+	element.preventDefault();
+	const modalOptions = {
+		onShow: () => {
+			window.addEventListener('keydown', onEscapeKeyDown);
+		},
+		onClose: () => {
+			window.removeEventListener('keydown', onEscapeKeyDown);
+		},
+    };
+    
+	const modal = basicLightbox.create(`<img  src="${element.target.dataset.source}"width="1400" height="900">`, modalOptions);
+
+	function onEscapeKeyDown(element) {
+		if (element.code === 'Escape') {
+			modal.close();
+		}
+	}
+	modal.show();
+};
+
+
+
+
+
+
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,34 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+function makeGalleryImgMarkup({ preview, original, description }) {
+	return `<li>
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a></li>`;
+}
+
+function makeGalleryMarkup(array) {
+	return array.map(makeGalleryImgMarkup).join('');
+}
+function renderMarkup(markup) {
+	galleryEl.insertAdjacentHTML('afterbegin', markup);
+}
+const galleryMarkup = makeGalleryMarkup(galleryItems);
+renderMarkup(galleryMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+    overlayOpacity: 0.9,
+
+});
+
+// console.log(lightbox);
